aba5cb7c25a1f07bd093ffbb3fa4e8ea7f10bdf2
Kyle <polysplitter@gmail.com>
2019-01-05 21:11:11 -0600

    add success / failure pages

--- app.js
+++ app.js
@@ -16,7 +16,41 @@ app.post('/', (req, res, next) => {
     let lastname = req.body.lastname
     let email = req.body.email
 
-    console.log(`${firstname} ${lastname} ${email}`)
+    let data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            }
+        ]
+    }
+
+    let jsonData = JSON.stringify(data)
+
+    let options = {
+        url: `https://us7.api.mailchimp.com/3.0/lists/${config.uniqueID}`,
+        method: 'POST',
+        headers: {
+            "Authorization": `kyle ${config.key}`
+        },
+        body: jsonData
+    }
+
+    request(options, (error, response, body) => {
+        if(error) {
+            res.sendFile(__dirname + "/failure.html")
+        } else {
+            if(response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html")
+            } else {
+                res.sendFile(__dirname + "/failure.html")
+            }
+        }
+    })
 })
 
 module.exports = app
